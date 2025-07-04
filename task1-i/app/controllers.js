c25a2a9ac5c852750a627a81a3318f6496059522
Aaditya Kumar Mittal <adityakumarashish1909@gmail.com>
2025-07-04 11:31:46 +0530

    feat: Completed a basic application in AngularJS

---
+++ task1-i/app/controllers.js
@@ -0,0 +1,8 @@
+app.controller("MainController", [
+  "$scope",
+  function ($scope) {
+    $scope.title = "Welcome to my Basic Angular Application";
+    $scope.description =
+      "This is a basic AngularJS setup with a controller and scope variables.";
+  },
+]);
